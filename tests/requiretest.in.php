e0f65f1e9792b9d76812ac47d230c432a1f2bb4f
Sameera Lakshitha <lakshithasameera89@gmail.com>
2018-03-09 20:04:48 +0530

    Update requiretest.in.php

--- tests/requiretest.in.php
+++ tests/requiretest.in.php
@@ -1,17 +1,11 @@
 <?php
-                     
-      
-        
-          
-            
-               
-                 include "requiretest.help.php"; 
+   include "requiretest.help.php"; 
                    
-    include_once "requiretest.help.php";                
+   include_once "requiretest.help.php";                
                      
-                  require "requiretest.help.php";             
+   require "requiretest.help.php";             
                          
-                     require_once "requiretest.help.php";      
+   require_once "requiretest.help.php";      
                            
                                
                                
